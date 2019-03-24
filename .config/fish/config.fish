405936afa4bca9867217d1c6d7ad518d5c8fdbee
Michael Pilquist <mpilquist@gmail.com>
2019-03-24 12:14:41 -0400

    Removed git info, vi mode, and cwd from prompt in favor of iTerm 3.3 status bar

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,26 +9,5 @@ if not functions -q fisher
     fish -c fisher
 end
 
-function fish_right_prompt -d "Write out the right prompt"
-    # Do nothing if not in vi mode
-    if test "$fish_key_bindings" = "fish_vi_key_bindings"
-        or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
-        switch $fish_bind_mode
-            case default
-                set_color --bold --background red white
-                echo '[N]'
-            case insert
-                set_color normal
-                echo '[I]'
-            case replace_one
-                set_color --bold --background green white
-                echo '[R]'
-            case visual
-                set_color --bold --background magenta white
-                echo '[V]'
-        end
-        set_color normal
-        echo -n ' '
-    end
-end
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
